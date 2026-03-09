7b1e1eb812b97906e63be0cdb8270305dec98b39
MaksymHolovniak <maksum.golovnyak@gmail.com>
2026-03-09 15:04:37 +0200

    feat: implement authentication with access and refresh tokens

---
+++ backend/routes/product.route.js
@@ -0,0 +1,13 @@
+import express from "express";
+import {
+    getAllProducts,
+    getFeaturedProducts
+} from "../controllers/product.controller.js";
+import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
+
+const router = express.Router();
+
+router.get("/", protectRoute, adminRoute, getAllProducts);
+router.get("/featured", getFeaturedProducts);
+
+export default router;
---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv"; 
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.route.js";
+
+import { connectDB } from "./lib/db.js";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+
+app.listen(PORT, () => {
+    console.log("Server is running on http://localhost:" + PORT);
+    
+    connectDB();
+});
