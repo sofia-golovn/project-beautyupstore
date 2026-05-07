5bdf2d33789c57c8d1f7819a3919ba81cde28e3d
Sofiia Holovniak <_email@example.com>
2026-05-08 02:06:13 +0300

    finished work on the website

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv"; 
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
+
 
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
@@ -20,8 +22,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(cors({
-    origin: "http://localhost:5173", 
+    origin: process.env.CLIENT_URL || "http://localhost:5173", 
     credentials: true,
 }));
 
@@ -29,7 +33,6 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "15mb", extended: true }));
 app.use(cookieParser());
 
-// Роути
 app.use("/api/categories", categoryRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/auth", authRoutes);
@@ -49,6 +52,14 @@ app.use((err, req, res, next) => {
     }
     res.status(500).json({ message: "Something went wrong on the server" });
 });
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(PORT, async () => {
     console.log("Server is running on http://localhost:" + PORT);
